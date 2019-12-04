f9771a6f79ed8d1b2ef49209510ee6147564e1e4
ymmmtym <h.yukihisa.378@gmail.com>
2019-12-04 22:21:09 +0900

    second commit

--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
 variable "GCP_CREDENTIALS" {}
-variable "PROJECT_ID" {}
-
+variable "PROJECT_ID"      {}
 
 provider "google" {
-  credentials = "var.GCP_CREDENTIALS"
-  project     = "var.PROJECT_ID"
+  credentials = ${var.GCP_CREDENTIALS}
+  project     = ${var.PROJECT_ID}
   region      = "us-west1"
 }
