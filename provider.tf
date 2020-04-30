4e83de19147f981d2c24078dfd6829e1ea596df9
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-30 10:03:46 +0900

    modify syntax

--- provider.tf
+++ provider.tf
@@ -4,9 +4,8 @@ variable "REGION" {
   default = "us-west1"
 }
 
-
 provider "google" {
-  credentials = var.GCP_CREDENTIALS
-  project     = var.PROJECT_ID
-  region      = var.REGION
+  credentials = "${var.GCP_CREDENTIALS}"
+  project     = "${var.PROJECT_ID}"
+  region      = "${var.REGION}"
 }
